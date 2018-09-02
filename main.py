0e578e38ed9ca7ef5e277cef08f3963ca78053e7
itaigat <itaigat.mail@gmail.com>
2018-09-02 08:47:56 +0300

    timer bug fixed

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import time
 
 if __name__ == "__main__":
-    start_time_indexing = time.process_time()
-    # insert indexing functionality
-    end_time_indexing = time.process_time()
+    start_indexing_time = time.time()
+    """
+    
+    Insert indexing functionality
+    
+    """
+    end_indexing_time = time.time()
 
-    start_retrieval_time = time.process_time()
-    # insert retrieval functionality
-    end_retrieval_time = time.process_time()
+    start_retrieval_time = time.time()
+    '''
+    
+    Insert retrieval functionality
+    
+    '''
+    end_retrieval_time = time.time()
 
-    elapsed_time = time.process_time() - start_time_indexing
+    elapsed_index_time = end_indexing_time - start_indexing_time
+    elapsed_retrieval_time = end_retrieval_time - start_retrieval_time
+    elapsed_time = elapsed_retrieval_time + elapsed_retrieval_time
+
+    print('Total running time: ', elapsed_time)
+    print('Indexing running time: ', elapsed_index_time)
+    print('Retrieval running time: ', elapsed_retrieval_time)
